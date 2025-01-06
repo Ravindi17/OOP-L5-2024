0a1c61aaf0d1602628f9d3a84dfad8c8893934d8
Ravindi welagedara <ravindiwelagedara@gmail.com>
2025-01-06 17:08:29 +0530

    Delete ticketsystemBackend/ticketsystem directory

--- ticketsystemBackend/ticketsystem/src/main/java/com/TicketingSystem/repository/ConfigurationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.TicketingSystem.repository;
-
-import com.TicketingSystem.model.Configuration;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfigurationRepository extends JpaRepository<Configuration, Long> {
-}
